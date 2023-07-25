9857223949fd437e4c677bad445ae6fba7886721
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-07-26 00:13:23 +0800

    Change function to connect to GSheets

--- pages/3_Results.py
+++ pages/3_Results.py
@@ -14,9 +14,9 @@ def generate_plot_data(df):
         df.loc[row, "QNum"] = index["QNum"].replace("Q-", "")
     return df
 
+
 @st.cache_data
-def generate_streak_info(results, col:str = "Correct"):
-    
+def generate_streak_info(results, col: str = "Correct"):
     data = results[col].to_frame()
     data["start_of_streak"] = data[col].ne(data[col].shift())
     data["streak_id"] = data.start_of_streak.cumsum()
@@ -29,27 +29,47 @@ def generate_streak_info(results, col:str = "Correct"):
             df.loc[index, "Streak"] = 0
     return df
 
+
 @st.cache_data
 def generate_runchart(runchartdf):
-    runchart = alt.Chart(runchartdf).mark_point(filled=True, size=400).encode(
-        x=alt.X("QNum", title="Question Number", axis=alt.Axis(labelAngle=0)),
-        y=alt.Y("Correct", title=""),
-        color=alt.Color("Correct", scale=alt.Scale(domain=["Correct", "Incorrect"], range=["green", "red"]), legend=None),
-        tooltip=[alt.Tooltip("QNum", title="Question No"), alt.Tooltip("Correct", title="Status"), alt.Tooltip("Streak", title="Streak")],
-        shape=alt.Shape("Correct", scale=alt.Scale(domain=["Correct", "Incorrect"], range=['circle', 'triangle-up'])),
-    ).properties(
-        height=200
-    ).interactive()
+    runchart = (
+        alt.Chart(runchartdf)
+        .mark_point(filled=True, size=400)
+        .encode(
+            x=alt.X("QNum", title="Question Number", axis=alt.Axis(labelAngle=0)),
+            y=alt.Y("Correct", title=""),
+            color=alt.Color(
+                "Correct",
+                scale=alt.Scale(
+                    domain=["Correct", "Incorrect"], range=["green", "red"]
+                ),
+                legend=None,
+            ),
+            tooltip=[
+                alt.Tooltip("QNum", title="Question No"),
+                alt.Tooltip("Correct", title="Status"),
+                alt.Tooltip("Streak", title="Streak"),
+            ],
+            shape=alt.Shape(
+                "Correct",
+                scale=alt.Scale(
+                    domain=["Correct", "Incorrect"], range=["circle", "triangle-up"]
+                ),
+            ),
+        )
+        .properties(height=200)
+        .interactive()
+    )
 
     return runchart
+
 
 @st.cache_data
 def write_incorrect_questions(incorrect):
     incorrect_strings = []
     if st.session_state["auth"]:
         for index, row in incorrect.iterrows():
-            string = (
-                f"""
+            string = f"""
 <details><summary style='font-size: 1.2em;'>{row["QNum"].replace("Q-", "Question #")}</summary>
 <br>
 
@@ -62,12 +82,10 @@ def write_incorrect_questions(incorrect):
 <hr>
 </details>
                 """
-            )
             incorrect_strings.append(string)
     else:
         for index, row in incorrect.iterrows():
-            string = (
-                f"""
+            string = f"""
 <details><summary style='font-size: 1.2em;'>{row["QNum"].replace("Q-", "Question #")}</summary>
 <br>
 
@@ -80,16 +98,17 @@ def write_incorrect_questions(incorrect):
 <hr>
 </details>
                 """
-            )
             incorrect_strings.append(string)
     return incorrect_strings
 
-@st.cache_resource
-def connect_to_gsheets():
+
+@st.cache_resource(show_spinner="Loading...")
+def connect_to_gsheets(worksheet: str):
     gc = gspread.service_account_from_dict(st.secrets["GSHEETS_CREDS"])
     sh = gc.open("Board Review")
-    ws = sh.worksheet("PS Data")
-    return gc, ws
+    ws = sh.worksheet(worksheet)
+    return ws
+
 
 st.set_page_config(page_title="Results :: Problem Set Generator", page_icon="🏆")
 
@@ -100,7 +119,7 @@ if not st.session_state["problem_set"]["Done"].all():
     st.error("**Please complete the quiz first!**", icon="❗")
     st.stop()
 
-gc, ws = connect_to_gsheets()
+ws = connect_to_gsheets("PS Data")
 
 results = generate_streak_info(st.session_state["problem_set"].copy(), col="Correct")
 # results
@@ -112,11 +131,13 @@ with st.expander("Result Summary", expanded=True):
     with rs1:
         st.metric("Score", f"{sum(results['Correct'])} / {len(results)}")
     with rs2:
-        st.metric("Accuracy", f"{round(sum(results['Correct']) / len(results) * 100, 2)}%")
+        st.metric(
+            "Accuracy", f"{round(sum(results['Correct']) / len(results) * 100, 2)}%"
+        )
     with rs3:
         # Show metric for highest streak of correct questions from cumulative sum of correct answers column
         st.metric("Highest Streak", f"{max(results['Streak'])}")
-        
+
 
 # Generate altair scatter plot of correct and incorrect items vs question number, change symbol to checkmark or cross
 st.markdown("#### Run Chart")
@@ -132,8 +153,10 @@ incorrect = results[results["Correct"] == False]
 incorrect = incorrect[["ID", "QNum", "Question", "Answer", "Tags"]]
 incorrect_qid_list = [str(id) for id in incorrect["ID"].tolist()].copy()
 incorrect_qid_list.sort()
-incorrect_qid_list = '; '.join(incorrect_qid_list)
-incorrect_tags_list = '; '.join(list(set([item for sublist in incorrect["Tags"].tolist() for item in sublist])))
+incorrect_qid_list = "; ".join(incorrect_qid_list)
+incorrect_tags_list = "; ".join(
+    list(set([item for sublist in incorrect["Tags"].tolist() for item in sublist]))
+)
 incorrect_strings = write_incorrect_questions(incorrect)
 for q_string in incorrect_strings:
     st.markdown(q_string, unsafe_allow_html=True)
@@ -142,23 +165,32 @@ with st.sidebar:
     if st.session_state["auth"]:
         with st.form("Save Results"):
             duration = st.number_input("Run Duration (secs)", min_value=1, step=1)
-            run_tags = '; '.join(list(set([item for sublist in results["Tags"].tolist() for item in sublist])))
+            run_tags = "; ".join(
+                list(
+                    set(
+                        [
+                            item
+                            for sublist in results["Tags"].tolist()
+                            for item in sublist
+                        ]
+                    )
+                )
+            )
 
             result_list = [
                 dt.datetime.now().strftime("%Y-%m-%d"),
-                sum(results['Correct']),
+                sum(results["Correct"]),
                 len(results),
                 duration,
                 '=(INDIRECT("RC[-3]",FALSE))/INDIRECT("RC[-2]",FALSE)',
                 '=(INDIRECT("RC[-4]",FALSE)+1)/(INDIRECT("RC[-3]",FALSE)+2)',
-                max(results['Streak']),
+                max(results["Streak"]),
                 run_tags,
                 incorrect_qid_list,
-                incorrect_tags_list
+                incorrect_tags_list,
             ]
 
             if st.form_submit_button("Save Results"):
                 ws.append_rows([result_list], value_input_option="USER_ENTERED")
                 st.balloons()
                 st.toast("**Results saved!**", icon="🎉")
-
