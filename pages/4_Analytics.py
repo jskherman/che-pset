bee75c98b422e7ca715782005ff2234c5033d242
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-07-26 00:29:03 +0800

    Fix error message

--- pages/4_Analytics.py
+++ pages/4_Analytics.py
@@ -7,6 +7,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Analytics :: Problem Set Generator", page_icon="📊")
 
-if not st.session_state["auth"]:
-    st.error("&ensp; **You are unauthorized to see this page.** Please login.", icon="🔒")
+if ("auth" not in st.session_state) or (not st.session_state["auth"]):
+    st.error(
+        "&ensp; **You are unauthorized to see this page.** Please login.", icon="🔒"
+    )
     st.stop()
